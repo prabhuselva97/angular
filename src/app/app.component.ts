2bc53541c270fcee1993cc6bc3a018b44e83d9b5
Prabhu S <prabhu@virtusa.com>
2018-06-19 17:31:47 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // private _url: string ="https://form-cefa5.firebaseio.com/data.json";
+  constructor(private _http: Http){
+    
+
+  }
+ 
+  onSubmit(value: any){
+    // const data=value;
+    
+    const url = 'https://form-cefa5.firebaseio.com/data.json';
+    this._http.post(url,value)
+    .subscribe(rsp => console.log(rsp));
+    console.log("Posted");
+  }
 }
